29e12538b2d1c0f876e151181260af37578816eb
Lilis Iskandar <lilis@veand.co>
2018-07-23 14:31:17 +0800

    Initial commit
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

---
+++ examples/multiple.rs
@@ -0,0 +1,69 @@
+extern crate porcupine_sys;
+
+// Std
+use std::cmp::min;
+use std::fs::File;
+use std::io::Read;
+
+use porcupine_sys as pv;
+
+fn read_audio_file() -> Vec<u8> {
+    let mut file = File::open("assets/multiple.raw").unwrap();
+    let mut audio_u8 = Vec::new();
+
+    // Read file to memory
+    file.read_to_end(&mut audio_u8).unwrap();
+
+    audio_u8
+}
+
+fn vec_u8_to_vec_i16(input: &[u8]) -> Vec<i16> {
+    let mut output = Vec::new();
+    let mut i = 0;
+
+    while i < input.len() {
+        let h = (input[i + 1] as u16) << 8;
+        let l = input[i] as u16;
+        let val = (h | l) as i16;
+        output.push(val);
+        i += 2;
+    }
+
+    output
+}
+
+fn main() {
+    let audio_u8 = read_audio_file();
+    let audio_i16 = vec_u8_to_vec_i16(&audio_u8);
+    let frame_length = unsafe { pv::frame_length() };
+    let mut object = unsafe {
+        pv::Object::new_multiple_keywords(
+            "assets/porcupine_params.pv",
+            &[
+                "assets/play music_linux.ppn",
+                "assets/next music_linux.ppn",
+                "assets/stop music_linux.ppn",
+            ],
+            &[0.5, 0.5, 0.5],
+        ).unwrap()
+    };
+    let mut index = 0;
+
+    // Detect keyword
+    while index < audio_i16.len() {
+        let start = index;
+        let end = min(audio_i16.len(), index + frame_length);
+        let audio = &audio_i16[start..end];
+        let detected = unsafe { object.process_multiple_keywords(audio).unwrap() };
+
+        if detected >= 0 {
+            println!("Detected keyword at {}!", detected);
+        }
+
+        index += frame_length;
+    }
+
+    unsafe {
+        object.delete();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+//! # porcupine-sys
+//!
+//! A Rust binding for Porcupine
+
+// libc
+extern crate libc;
+use libc::c_int;
+
+mod c;
+
+// std
+use std::ffi::{CStr, CString};
+use std::fmt::{self, Display};
+use std::ptr;
+
+/// The status code returned by various porcupine functions.
+#[derive(Debug)]
+pub enum Status {
+    Success = 0,
+    OutOfMemory = 1,
+    IOError = 2,
+    InvalidArgument = 3,
+    Unknown = 4,
+}
+
+/// Audio sample rate accepted by Picovoice.
+pub unsafe fn sample_rate() -> usize {
+    c::pv_sample_rate() as usize
+}
+
+/// Version of Porcupine.
+pub unsafe fn version() -> &'static str {
+    CStr::from_ptr(c::pv_porcupine_version()).to_str().unwrap()
+}
+
+/// Length (number of audio samples) per frame.
+pub unsafe fn frame_length() -> usize {
+    c::pv_porcupine_frame_length() as usize
+}
+
+pub struct Object {
+    _object: *mut c::pv_porcupine_object_t,
+}
+
+impl Object {
+    /// Creates a new Porcupine object.
+    pub unsafe fn new(
+        model_file_path: &str,
+        keyword_file_path: &str,
+        sensitivity: f32,
+    ) -> Result<Self, Status> {
+        let mut _object: *mut c::pv_porcupine_object_t = ptr::null_mut();
+        let _model_file_path = CString::new(model_file_path).unwrap().into_raw();
+        let _keyword_file_path = CString::new(keyword_file_path).unwrap().into_raw();
+
+        let status = c::pv_porcupine_init(
+            _model_file_path,
+            _keyword_file_path,
+            sensitivity,
+            &mut _object,
+        );
+        if status != 0 {
+            return Err(status.into());
+        }
+
+        Ok(Object { _object })
+    }
+
+    /// Creates a new Porcupine object that is capable of detecting multiple keywords.
+    pub unsafe fn new_multiple_keywords(
+        model_file_path: &str,
+        keyword_file_paths: &[&str],
+        sensitivities: &[f32],
+    ) -> Result<Self, Status> {
+        let mut _object: *mut c::pv_porcupine_object_t = ptr::null_mut();
+        let _model_file_path = CString::new(model_file_path).unwrap().into_raw();
+        let _number_keywords = keyword_file_paths.len() as c_int;
+        let _keyword_file_paths: Vec<CString> = keyword_file_paths
+            .iter()
+            .map(|p| CString::new(*p).unwrap())
+            .collect();
+        let _keyword_file_paths: Vec<_> = _keyword_file_paths.iter().map(|p| p.as_ptr()).collect();
+
+        let status = c::pv_porcupine_multiple_keywords_init(
+            _model_file_path,
+            _number_keywords,
+            _keyword_file_paths.as_ptr() as *const *const i8,
+            sensitivities.as_ptr(),
+            &mut _object,
+        );
+        if status != 0 {
+            return Err(status.into());
+        }
+
+        Ok(Object { _object })
+    }
+
+    /// Delete the Porcupine object.
+    pub unsafe fn delete(&mut self) {
+        c::pv_porcupine_delete(self._object);
+    }
+
+    /// Detect keyword within the provided audio data. The data must be 16-bit linearly-encoded and single-channel with sample rate equal to `sample_rate()`.
+    pub unsafe fn process(&self, pcm: &[i16]) -> Result<bool, Status> {
+        let mut detected = false;
+
+        let status = c::pv_porcupine_process(self._object, pcm.as_ptr(), &mut detected);
+        if status != 0 {
+            return Err(status.into());
+        }
+
+        Ok(detected)
+    }
+
+    /// Detect one of the keywords within the provided audio data. The data must be 16-bit linearly-encoded and single-channel with sample rate equal to `sample_rate()`. It returns the index of the detected keyword if successful.
+    pub unsafe fn process_multiple_keywords(&self, pcm: &[i16]) -> Result<isize, Status> {
+        let mut keyword_index: c_int = -1;
+
+        let status = c::pv_porcupine_multiple_keywords_process(
+            self._object,
+            pcm.as_ptr(),
+            &mut keyword_index,
+        );
+        if status != 0 {
+            return Err(status.into());
+        }
+
+        Ok(keyword_index as isize)
+    }
+}
+
+impl From<c::pv_status_t> for Status {
+    fn from(status: c::pv_status_t) -> Self {
+        match status {
+            0 => Status::Success,
+            1 => Status::OutOfMemory,
+            2 => Status::IOError,
+            3 => Status::InvalidArgument,
+            _ => Status::Unknown,
+        }
+    }
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Status::Success => write!(f, "Success"),
+            Status::OutOfMemory => write!(f, "Out Of Memory"),
+            Status::IOError => write!(f, "I/O Error"),
+            Status::InvalidArgument => write!(f, "Invalid Argument"),
+            _ => write!(f, "Unknown"),
+        }
+    }
+}
